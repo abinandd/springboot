24b584a082fc55462d20433b2bac1e5f9375584a
abhinand <abhinand155@gmail.com>
2026-05-18 16:01:52 +0530

    chore: added env, connected db

--- ProductStore/src/main/java/com/products/productstore/ProductStoreApplication.java
+++ ProductStore/src/main/java/com/products/productstore/ProductStoreApplication.java
@@ -1,5 +1,6 @@
 package com.products.productstore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductStoreApplication {
 
     public static void main(String[] args) {
+
+        // Taking informations from the env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((dotenvEntry -> System.setProperty(
+                dotenvEntry.getKey(),dotenvEntry.getValue()
+        )));
         SpringApplication.run(ProductStoreApplication.class, args);
     }
-
 }
